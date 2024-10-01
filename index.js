7e5c3008602d67a1c98995da419029509dae8a56
Demostat101 <demostat1995@gmail.com>
2024-10-01 04:00:44 +0100

    set cors for specific url link in index.js file

--- index.js
+++ index.js
@@ -8,10 +8,33 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3500;
 
 const app = express();
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://hr360dashboard-omikunle-ademola.netlify.app',
+  ];
+  const corsOptions = {
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, Postman)
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'OPTIONS',"PATCH","DELETE"], // Allowed methods
+    allowedHeaders: ['Content-Type'], // Allowed headers
+    credentials: true, // If you need to include credentials
+  };
+  
+  // Use CORS middleware
+  app.use(cors(corsOptions));
+  
+  // Handle OPTIONS method for preflight requests
+  app.options('*', cors(corsOptions)); // Enable preflight across all routes
 
 
 app.use(bodyParser.json());
-app.use(cors({origin:"*", optionsSuccessStatus:200}));
+// app.use(cors({origin:"*", optionsSuccessStatus:200}));
 
 
 app.use("/", routes);
